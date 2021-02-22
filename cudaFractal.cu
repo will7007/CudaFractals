9cc1e6057dfdedfce1da58f27fdbbfd3f7262f20
will <will7007@gmail.com>
2021-02-21 23:35:17 -0500

    Keyboard input added, time to explore!

--- cudaFractal.cu
+++ cudaFractal.cu
@@ -1,5 +1,5 @@
 #include <iostream>
-#include <fstream> // for files manipulation
+//#include <fstream> // for files manipulation
 #include <cstdlib>
 #include <thrust/complex.h>
 #include <GL/glut.h>
@@ -22,9 +22,9 @@ struct vec3 { //dim3's are only for ints, so this one is for floats
 const int threadDim = 32;
 const dim3 blockSize(threadDim, threadDim, 1);
 const dim3 gridSize(width/threadDim + 1, height/threadDim + 1, 1);
-const dim3 dimsVar = dim3(width,height);
-const vec3 imDim = vec3{imWidth,imHeight}; //fixed: dim3's rouding down 1 (0.999) causing div by 0 in GPU
-const vec3 imEdge = vec3{imEdgeX,imEdgeY};
+dim3 dimsVar = dim3(width,height);
+vec3 imDim = vec3{imWidth,imHeight}; //fixed: dim3's rouding down 1 (0.999) causing div by 0 in GPU
+vec3 imEdge = vec3{imEdgeX,imEdgeY};
 int *d_red, *d_green, *d_blue;
 int *h_red, *h_green, *h_blue;
 
@@ -32,8 +32,12 @@ int *h_red, *h_green, *h_blue;
 
 __device__ vec3 fractalCalculator(thrust::complex<float> z,thrust::complex<float> c, int iterLimit) {
     int iter = 0;
-    while(thrust::abs(z) < 2.0 && iter <= iterLimit) {
-        z = z*z + c;
+    while(thrust::abs(z) < 2.0 && iter <= iterLimit) { //4 for ship
+        z = z*z + c; //mandlebrot
+//        float zReal = z.real(), zImag = z.imag();
+//        thrust::complex<float> zShip(zReal < 0 ? -1*zReal : zReal,
+//                                     zImag < 0 ? -1*zImag : zImag);
+//        z = zShip*zShip + c;
         iter++;
     }
     vec3 returnValue;
@@ -44,6 +48,7 @@ __device__ vec3 fractalCalculator(thrust::complex<float> z,thrust::complex<float
 }
 
 __device__ float dims(int pos, int dim, float imDim, float imEdge) {
+    //linearly maps x values to [-1.5,-0.5] and y to [-0.5,-0.5]
     return static_cast<float>(pos)/static_cast<float>(dim)*imDim + imEdge;
 }
 
@@ -56,7 +61,6 @@ __global__ void fractal(int *red, int *green, int *blue, dim3 dimens, vec3 imDim
     // But you only hear about it once you try to copy the memory back to the host
     thrust::complex<float> c(dims(x,dimens.x,imDims.x,imEdges.x), //returns INT_MAX
                              dims(y,dimens.y,imDims.y,imEdges.y)); //returns 0
-    //linearly maps x values to [-1.5,-0.5] and y to [-0.5,-0.5]
     thrust::complex<float> z(0, 0); //must declare precision of complex #
     vec3 result = fractalCalculator(z,c, iterLimit);
     int absScaled = static_cast<int>(result.y/5.0*INT_MAX);
@@ -65,16 +69,6 @@ __global__ void fractal(int *red, int *green, int *blue, dim3 dimens, vec3 imDim
     green[y*dimens.y + x] = absScaled;
     blue[y*dimens.y + x] = iterScaled;
 }
-
-//void ppmWriter(int width, int height, int *red, int *green, int *blue) {
-//    ofstream my_Image ("mandelbrot.ppm");
-//    if(!my_Image.is_open()) { return; }
-//    my_Image << "P3\n" << width << " " << height << " 255\n";
-//    for(int i = 0; i < height*width; i++) {
-//        my_Image << red[i]/INT_MAX*255 << ' ' << green[i]/INT_MAX*255 << ' ' << blue[i]/INT_MAX*255 << "\n";
-//    }
-//    my_Image.close();
-//}
 
 void sayError() {
     cudaError_t cudaResult = cudaGetLastError();
@@ -86,8 +80,87 @@ void sayError() {
 }
 
 void renderLoop() {
+    glClear(GL_DEPTH_BUFFER_BIT);
     glDrawPixels(width,height,GL_RED,GL_INT,h_green);
     glutSwapBuffers();
+}
+
+void runKernel() {
+    cout << "Running kernel..." << endl;
+    dimsVar = dim3(width,height);
+    imDim = vec3{imWidth,imHeight}; //fixed: dim3's rouding down 1 (0.999) causing div by 0 in GPU
+    imEdge = vec3{imEdgeX,imEdgeY};
+    fractal<<<gridSize,blockSize>>>(d_red,d_green,d_blue,dimsVar,imDim,imEdge,iterLimit);
+    cudaMemcpy(h_red,d_red,width*height*sizeof(int),cudaMemcpyDeviceToHost);
+    cudaMemcpy(h_green,d_green,width*height*sizeof(int),cudaMemcpyDeviceToHost);
+    cudaMemcpy(h_blue,d_blue,width*height*sizeof(int),cudaMemcpyDeviceToHost);
+    sayError();
+}
+
+void keyboard(unsigned char key, int x, int y) {
+    switch (key) {
+        case 97:
+            imEdgeX -= imWidth*.01;
+            cout << "Left: imEdgeX is now " << imEdgeX << endl;
+            break;
+        case 100:
+            imEdgeX += imWidth*.01;
+            cout << "Right: imEdgeX is now " << imEdgeX << endl;
+            break;
+        case 119:
+            imEdgeY += imHeight*.01;
+            cout << "Up: imEdgeY is now " << imEdgeY << endl;
+            break;
+        case 115:
+            imEdgeY -= imHeight*.01;
+            cout << "Down: imEdgeY is now " << imEdgeY << endl;
+            break;
+        case 65:
+            imEdgeX -= imWidth*.1;
+            cout << "Super left: imEdgeX is now " << imEdgeX << endl;
+            break;
+        case 68:
+            imEdgeX += imWidth*.1;
+            cout << "Super right: imEdgeX is now " << imEdgeX << endl;
+            break;
+        case 87:
+            imEdgeY += imHeight*.1;
+            cout << "Super up: imEdgeY is now " << imEdgeY << endl;
+            break;
+        case 83:
+            imEdgeY -= imHeight*.1;
+            cout << "Super down: imEdgeY is now " << imEdgeY << endl;
+            break;
+        case 102: //f
+            imWidth /= 1.2;
+            imHeight /= 1.2;
+            cout << "Focusing in: imWidth is now " << imWidth << endl;
+            break;
+        case 118: //v
+            imWidth *= 1.2;
+            imHeight *= 1.2;
+            cout << "Focusing out: imWidth is now " << imWidth << endl;
+            break;
+        case 70: //f
+            imWidth /= 2;
+            imHeight /= 2;
+            cout << "Super focusing in: imWidth is now " << imWidth << endl;
+            break;
+        case 86: //v
+            imWidth *= 2;
+            imHeight *= 2;
+            cout << "Super focusing out: imWidth is now " << imWidth << endl;
+            break;
+        case 113: //press q to quit
+            glutDestroyWindow(glutGetWindow());
+            return;
+            break;
+        default:
+            cout << "Default key case" << endl;
+            break;
+    }
+    runKernel();
+    glutPostRedisplay();
 }
 
 int main (int argc, char *argv[])  {
@@ -102,17 +175,11 @@ int main (int argc, char *argv[])  {
         default:
             cout << "No args given, using defaults" << endl;
             break;
-        case 8:
-            iterLimit = atof(argv[7]);
-        case 7:
-            width = atof(argv[5]);
-            height = atof(argv[6]);
-        case 5:
-            imEdgeX = atof(argv[3]);
-            imEdgeY = atof(argv[4]);
+        case 4:
+            iterLimit = atof(argv[3]);
         case 3:
-            imWidth = atof(argv[1]);
-            imHeight = atof(argv[2]);
+            width = atof(argv[1]);
+            height = atof(argv[2]);
             break;
 	}
 
@@ -120,19 +187,12 @@ int main (int argc, char *argv[])  {
 	cudaMalloc(&d_red,width*height*sizeof(int));
     cudaMalloc(&d_green,width*height*sizeof(int));
     cudaMalloc(&d_blue,width*height*sizeof(int));
-
-
-    fractal<<<gridSize,blockSize>>>(d_red,d_green,d_blue,dimsVar,imDim,imEdge,iterLimit);
-
     h_red = new int[width*height];
     h_green = new int[width*height];
     h_blue = new int[width*height];
-    cudaMemcpy(h_red,d_red,width*height*sizeof(int),cudaMemcpyDefault);
-    cudaMemcpy(h_green,d_green,width*height*sizeof(int),cudaMemcpyDefault);
-    cudaMemcpy(h_blue,d_blue,width*height*sizeof(int),cudaMemcpyDefault);
 
-//    ppmWriter(width,height,h_red,h_green,h_blue);
-    sayError();
+    runKernel();
+    glutKeyboardFunc(keyboard);
     glutDisplayFunc(renderLoop);
     glutMainLoop();
 
